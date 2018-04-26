3f23807668abd76b89d77e6f810a58feb80aa9e3
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-04-26 10:24:06 -0400

    Refactor App to be stateless

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* global it, expect */
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
